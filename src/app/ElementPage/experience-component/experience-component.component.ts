b03259c7c478f90d82ff6f910f5898e04f6de131
nuttawwn <nuttawutok111@gmail.com>
2023-03-19 21:15:53 +0700

    update exprerance component

--- src/app/ElementPage/experience-component/experience-component.component.ts
+++ src/app/ElementPage/experience-component/experience-component.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-experience-component',
   templateUrl: './experience-component.component.html',
   styleUrls: ['./experience-component.component.scss']
 })
-export class ExperienceComponentComponent {
+export class ExperienceComponentComponent implements OnInit{
+  public toggle: boolean = true;
+  public alternate: boolean = true;
+  public color: boolean = true;
+  public dotAnimation: boolean = true;
+  public expandEnabled: boolean = true;
+  public contentAnimation: boolean = true;
+  public size: number = 40;
+  public side = 'left';
+  constructor(){
 
+  }
+ngOnInit(): void {
+
+}
+  public onDotClick(event : any) {
+    if (!this.expandEnabled) {
+      event.stopPropagation();
+    }
+  }
+  public onHeaderClick(event : any) {
+    if (!this.expandEnabled) {
+      event.stopPropagation();
+    }
+  }
 }
