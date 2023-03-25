8547ce7b2066960f36a79004ac1c862b9f7267f9
nuttawwn <nuttawutok111@gmail.com>
2023-03-25 11:44:24 +0700

    update detect window screen

--- src/app/ElementPage/experience-component/experience-component.component.ts
+++ src/app/ElementPage/experience-component/experience-component.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-experience-component',
   templateUrl: './experience-component.component.html',
   styleUrls: ['./experience-component.component.scss']
 })
-export class ExperienceComponentComponent implements OnInit{
+export class ExperienceComponentComponent implements OnInit,OnChanges{
   public toggle: boolean = true;
   public alternate: boolean = true;
   public color: boolean = true;
@@ -14,10 +14,26 @@ export class ExperienceComponentComponent implements OnInit{
   public contentAnimation: boolean = true;
   public size: number = 40;
   public side = 'left';
+  public disPreriodTime : boolean = false;
+  public bodyscreen = document.getElementsByClassName("experience")[0];
+
   constructor(){
 
   }
-ngOnInit(): void {
+
+ngOnInit(){
+  // if(window.screen.width <=900){
+  //   this.disPreriodTime = true
+  // }
+  window.screen.width <= 500 ? this.disPreriodTime = true : this.disPreriodTime = false
+  this.detectScreen(window.screen.width)
+
+}
+ngOnChanges(changes: SimpleChanges){
+  console.log("changes = ",changes);
+  if(changes){
+    window.screen.width <= 500 ? this.disPreriodTime = true : this.disPreriodTime = false
+  }
 
 }
   public onDotClick(event : any) {
@@ -29,5 +45,13 @@ ngOnInit(): void {
     if (!this.expandEnabled) {
       event.stopPropagation();
     }
+  }
+  public detectScreen (num : number){
+    let width = num
+    window.onresize
+    let reSizeWindow = new ResizeObserver(() =>{
+      width = window.innerWidth;
+    })
+    reSizeWindow.observe(this.bodyscreen);
   }
 }
